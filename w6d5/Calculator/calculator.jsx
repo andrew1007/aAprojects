a81ced8bfbadcca9b417cc9fe6ba3842204794ee
andrew1007 <yueh.andrew@gmail.com>
2016-12-23 09:02:31 -0800

    calculator

--- w6d5/Calculator/calculator.jsx
+++ w6d5/Calculator/calculator.jsx
@@ -3,15 +3,42 @@ import React from 'react';
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    // your code here
+    this.state = {firstNum: "", secondNum: "", result: 0};
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+    this.add = this.add.bind(this);
   }
 
-  // your code here
+  setNum1(e){
+    const firstNum = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({firstNum})
+  }
+
+  setNum2(e){
+    const secondNum = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({secondNum})
+  }
+
+  add(e){
+    e.preventDefault();
+    let result = this.state.firstNum + this.state.secondNum;
+    this.setState({result});
+  }
+
+  clear(e){
+    e.preventDefault();
+    this.setState(firstNum: "", secondNum: "", result: 0);
+  }
 
   render() {
+    const {firstNum, secondNum, result} = this.state
     return (
       <div>
-        <h1>Hello World</h1> // replace this with your code
+        <h1>{result}</h1> // replace this with your code
+        <input onChange={this.setNum1} value={firstNum}/>
+        <input onChange={this.setNum2} value={secondNum}/>
+        <button onClick={this.clear}>reset</button>
+        <button onClick={this.add}>add</button>
       </div>
     );
   }
